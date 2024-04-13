01ffb679e45d10aeff1c1c74294c79542185cda9
Johann Haslinger <heyjohn655@gmail.com>
2024-04-13 15:30:20 +0200

    updated Lyrics

--- src/assets/uschi/uschi-lyrics-tab.ts
+++ src/assets/uschi/uschi-lyrics-tab.ts
@@ -6,24 +6,547 @@ u
 u
 3
 u Du
-4 hast_
-u die__
+4 hast
+u die
 1|
-1 Ha___
-u -are____
-2 schön_____
+1 Ha_
+u -are
+2 schön
 u
 3
 u und
-4 machst_
-u die__
+4 machst
+u die
 2|
-1 Ha___
-u -are____
-2 schön_____
+1 Ha_
+u -are
+2 schön
 u
 3
-u 
-4 
 u
+4 Da
+u -rum
+3|
+1 Fiel
+u es
+2 mir
+u nicht
+3 schwer
+u mit
+4 dir'n
+u Ter
+4|
+1 -min 
+u zu
+2 re
+u -ser
+3 -viern
+u Du
+4 machst
+u den
+5|
+1 neu
+u -sten
+2 Schnitt_
+u
+3
+u mit
+4 H
+u 2
+6|
+1 Per
+u -o
+2 -xid_
+u 
+3
+u dank
+4 dei
+u -ner
+7| 
+1 French
+u -nail-
+2 -Zau
+u -ber
+3 -fing
+u -er
+4 machst
+u Du
+8|
+1 voll
+u den
+2 gu
+u -ten
+3 Schnitt!
+u
+4 Au!
+u
+9|
+1
+u Ma
+2 ma
+u ma
+3 ma
+u
+4 ma
+u ma
+10|
+1
+u
+2
+u
+3
+u
+4
+u
+11|
+1
+u ba
+2 ba
+u ba
+3 ba
+u 
+4 ba
+u ba
+12|
+1
+u
+2
+u
+3
+u
+4
+u
+13|
+1
+u Ma
+2 ma
+u ma
+3 ma
+u -ster
+4 Bar
+u -ber
+14|
+1 Ur
+u -su
+2 -la
+u
+3
+u
+4
+u
+15|
+1
+u
+2
+u
+3
+u
+4 Du
+u musst
+16|
+1 Haar_
+u 
+2 -trock
+u -nen,
+3 U
+u -schi, den
+4 gan
+u -zen
+17|
+1 Tag_
+u
+2 Lo
+u -cken
+3 wu
+u -scheln
+4 letz
+u -tes
+18|
+1 Hand
+u -werk
+2 mit
+u ner
+3 Fu
+u -ture
+4 wo
+u bis
+19|
+1 heu
+u -te
+2 kein
+u Com
+3 -pu
+u -ter
+4 dir
+u das
+20|
+1 Was
+u -ser
+2 für
+u die
+3 Spü
+u -lung
+4 rei
+u -chen
+21|
+1 kann.
+u
+2
+u
+3
+u
+4
+u
+22|
+1
+u
+2
+u
+3
+u
+4
+u
+23|
+1
+u
+2
+u
+3
+u
+4
+u
+24|
+1
+u
+2
+u
+3
+u Du
+4 hast
+u ver
+25|
+1 -wöhnt,
+u ge
+2 -tönt,
+u ge
+3 -föhnt,
+u nie
+4 groß
+u bei 
+26|
+1 Ü
+u -ber
+2 -last
+u ge
+3 -stöhnt_
+u
+4 Neun
+u -zehn
+27|
+1 -ein
+u -und
+2 -neun
+u -zig
+3 wur
+u -dest
+4 Du
+u zur
+28|
+1 Queen
+u of
+2 Hair 
+u ge
+3 -krönt.
+u In
+4 Ja
+u -cobs
+29|
+1 -dorf
+u
+2
+u
+3
+u bei
+4 Frank
+u -furt
+30|
+1 O
+u -der.
+2
+u
+3
+u
+4 Dei
+u -ne
+31|
+1 Kre
+u -haar
+2 -ti
+u -vi
+3 -tät
+u wird
+4 noch
+u im
+32|
+1 Ren
+u -ten
+2 -al
+u -ter
+3 lo
+u -dern.
+4
+u
+33|
+1
+u Ma
+2 ma
+u ma
+3 ma
+u
+4 ma
+u ma
+34|
+1
+u
+2
+u
+3
+u
+4
+u
+35|
+1
+u ba
+2 ba
+u ba
+3 ba
+u
+4 ba
+u ba
+36|
+1
+u
+2
+u
+3
+u
+4
+u
+37|
+1 
+u ma
+2 ma
+u ma
+3 Ma
+u -ster
+4 Bar
+u -ber
+38|
+1 Ur
+u -su
+2 la
+u
+3
+u
+4
+u
+39|
+1
+u
+2
+u
+3
+u
+4 Du
+u musst
+40|
+1 Haar_
+u
+2 trock
+u -nen,
+3 U
+u -schi, den
+4 gan
+u -zen
+41|
+1 Tag_
+u
+2 Lo
+u -cken
+3 wu
+u -scheln,
+4 letz
+u -tes
+42|
+1 Hand
+u -werk
+2 mit
+u ner
+3 Fu
+u -ture
+4 wo
+u bis
+43|
+1 heu
+u -te
+2 kein
+u Com
+3 -pu
+u -ter
+4 dir
+u das
+44| 
+1 Was
+u -ser
+2 für 
+u die
+3 Spü
+u -lung
+4 rei
+u -chen
+45|
+1 kann.
+u
+2
+u
+3
+u
+4
+u
+46|
+1
+u
+2
+u
+3
+u
+4
+u
+47|
+1
+u
+2
+u
+3
+u
+4
+u
+48|
+1
+u
+2
+u
+3
+u
+4
+u
+49|
+1
+u
+2
+u
+3
+u
+4
+u
+50|
+1
+u
+2
+u
+3
+u
+4
+u
+51|
+1
+u
+2
+u
+3
+u
+4
+u
+52|
+1
+u
+2
+u
+3
+u
+4
+u
+53|
+1
+u
+2
+u
+3
+u
+4
+u
+54|
+1
+u
+2
+u
+3
+u
+4
+u
+55|
+1
+u
+2
+u
+3
+u
+4
+u
+56|
+1
+u
+2
+u
+3
+u
+4
+u
+57|
+1
+u
+2
+u
+3
+u
+4
+u
+58|
+1
+u
+2
+u
+3
+u
+4
+u
+59|
+1
+u
+2
+u
+3
+u
+4
+u
+60|
+1
+u
+2
+u
+3
+u
+4
+u Ich
+
 `;
